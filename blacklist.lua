eff03bf42bf9226b8c601da255b56ba7ba0b2c1c
spoofy-raccoon <141374373+spoofy-raccoon@users.noreply.github.com>
2023-08-12 19:46:41 -0400

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,1 +1,10 @@
-
+userids = {
+[488987727] = true, 
+[USERIDHERE] = true,
+[USERIDHERE] = true,
+[USERIDHERE] = true,
+[USERIDHERE] = true,
+[USERIDHERE] = true,
+[USERIDHERE] = true
+}
+return userids
